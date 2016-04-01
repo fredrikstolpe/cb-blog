26c15d36c0e1fff5803f1400efc957b2b441ab27
Fredrik Stolpe <fredrik.stolpe@gmail.com>
2016-04-01 10:44:46 +0200

    Post date and tags

--- single.php
+++ single.php
@@ -10,6 +10,7 @@
               <header>
               <?php if ( is_singular() ) { echo '<h1 class="entry-title">'; } else { echo '<h2 class="entry-title">'; } ?><span><?php the_title(); ?></span><?php if ( is_singular() ) { echo '</h1>'; } else { echo '</h2>'; } ?>
               </header>
+              <div class="post-date">Posted <?php the_date(); ?> by <?php the_author(); ?><?php the_tags('<div class="tags"><span class="tag">','</span><span class="tag">','</span></div>');?></div>
               <?php get_template_part( 'entry', ( is_home() || is_archive() || is_search() ? 'summary' : 'content' ) ); ?>
             </div>
           </article>
